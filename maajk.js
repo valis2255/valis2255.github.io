2bf5bc4345b00b586da24eee8ed541d9c1d45cdd
valis2255 <v4l15@proton.me>
2024-07-18 20:27:57 +0200

    Update maajk.js

--- maajk.js
+++ maajk.js
@@ -1,34 +1,34 @@
 function bakeKaandy(key, value, daz) {
-    if (daz) {
-        var greg = new Date();
-        greg.setTime(greg.getTime() + (daz * 24 * 60 * 60 * 1000));
-        var expirez = "; expirez=" + greg.toGMTString();
-    } else var expirez = "";
-    document.cookie = key + "=" + value + expirez + "; path=/";
+   if (daz) {
+      var greg = new Date();
+      greg.setTime(greg.getTime() + (daz * 24 * 60 * 60 * 1000));
+      var expirez = "; expirez=" + greg.toGMTString();
+   } else var expirez = "";
+   document.cookie = key + "=" + value + expirez + "; path=/";
 }
 
 function munzhKaandy(key) {
-    var keyEQ = key + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(keyEQ) == 0) return c.substring(keyEQ.length, c.length);
-    }
-    return null;
+   var keyEQ = key + "=";
+   var ca = document.cookie.split(';');
+   for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(keyEQ) == 0) return c.substring(keyEQ.length, c.length);
+   }
+   return null;
 }
 
 function enzhaant(zkey) {
-    let hash = 0;
-    if (zkey.length == 0) return hash;
-    for (i = 0; i < zkey.length; i++) {
-        char = zkey.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash = hash & hash;
-    }
-    return hash;
+   let hash = 0;
+   if (zkey.length == 0) return hash;
+   for (i = 0; i < zkey.length; i++) {
+      char = zkey.charCodeAt(i);
+      hash = ((hash << 5) - hash) + char;
+      hash = hash & hash;
+   }
+   return hash;
 }
 
 function synk(zkey) {
-    return enzhaant(zkey) === 2113278331 || enzhaant(zkey) === -658996397;
+   return enzhaant(zkey) === 2113278331 || enzhaant(zkey) === -658996397;
 }
